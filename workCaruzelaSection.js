de1ec1f640ad80026d24577ff5ffae4a3d26b509
pawelsz93 <pawel.szmyd93@gmail.com>
2022-12-16 21:42:20 +0100

    work elements fade with pause

--- workCaruzelaSection.js
+++ workCaruzelaSection.js
@@ -5,28 +5,50 @@ const work2 = { element: works[1], index: 2 };
 const work3 = { element: works[2], index: 3 };
 const worksObjects = [work1, work2, work3];
 
-export function switchLeft() {
-  worksObjects.forEach(work => {
-    work.index--;
-    if (work.index == 0) work.index = 3;
-    if (work.index == 2) {
-      work.element.classList.add("caruzela__workElement--active");
-    } else {
-      work.element.classList.remove("caruzela__workElement--active");
-    }
-    work.element.style.order = work.index;
+function fadeOutAndIn(worksObjects) {
+  worksObjects.forEach(workObj => {
+    workObj.element.classList.toggle("caruzela__workElement--fadedOut");
   });
 }
 
+export function switchLeft() {
+  fadeOutAndIn(worksObjects);
+
+  setTimeout(() => {
+    worksObjects.forEach(work => {
+      work.index--;
+      if (work.index == 0) work.index = 3;
+      if (work.index == 2) {
+        work.element.classList.add("caruzela__workElement--active");
+      } else {
+        work.element.classList.remove("caruzela__workElement--active");
+      }
+      work.element.style.order = work.index;
+
+      setTimeout(() => {
+        fadeOutAndIn(worksObjects);
+      }, 300);
+    });
+  }, 300);
+}
+
 export function switchRight() {
-  worksObjects.forEach(work => {
-    work.index++;
-    if (work.index == 4) work.index = 1;
-    if (work.index == 2) {
-      work.element.classList.add("caruzela__workElement--active");
-    } else {
-      work.element.classList.remove("caruzela__workElement--active");
-    }
-    work.element.style.order = work.index;
-  });
+  fadeOutAndIn(worksObjects);
+
+  setTimeout(() => {
+    worksObjects.forEach(work => {
+      work.index++;
+      if (work.index == 4) work.index = 1;
+      if (work.index == 2) {
+        work.element.classList.add("caruzela__workElement--active");
+      } else {
+        work.element.classList.remove("caruzela__workElement--active");
+      }
+      work.element.style.order = work.index;
+
+      setTimeout(() => {
+        fadeOutAndIn(worksObjects);
+      }, 300);
+    });
+  }, 300);
 }
